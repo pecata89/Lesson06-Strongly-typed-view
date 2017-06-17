fd6a2f5997952853a783a4a90ced2ebdadaf98b7
Petko Petkov <petko.petkov@gmail.com>
2017-06-17 11:20:02 +0200

    Pagination added, with helpers and view models and styles of showing the products are improved.

--- MbmStore/Controllers/CatalogueController.cs
+++ MbmStore/Controllers/CatalogueController.cs
@@ -4,20 +4,37 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MbmStore.Models;
+using MbmStore.ViewModels;
 using MbmStore.Infrastructure;
 
 namespace MbmStore.Controllers
 {
     public class CatalogueController : Controller
     {
+        public int PageSize = 3;
 
         // object
         // private Repository repository = new Repository();
 
         // GET: Catalogue
-        public ActionResult Index()
+        public ActionResult Index(int page = 1)
         {
             Repository repository = new Repository();
+
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductId)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
 
             List<SelectListItem> Quantity = new List<SelectListItem>();
 
@@ -27,8 +44,8 @@ namespace MbmStore.Controllers
             }
 
             ViewBag.Quantity = Quantity;
-            
-            return View(repository.Products);
+
+            return View(model);
         }
     }
 }
